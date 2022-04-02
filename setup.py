77c27211839891e7849d33aed699ac50806c9543
1smaa <77068062+1smaa@users.noreply.github.com>
2022-04-02 21:01:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name="binarytree",
       author="Ismaele Lorenzon",
       packages=["binarytree"],
-      version="0.0.1",
+      version="0.1.0",
       author_email="lorenzonismaele@gmail.com",
       description="A Python package to create, search, store and load binary trees.",
       license="MIT License")
